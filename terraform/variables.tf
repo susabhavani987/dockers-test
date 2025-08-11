77b386bfd7fc8f7fe1202aac980725e591784f30
susabhavani987 <vijaychava101@gmail.com>
2025-08-11 17:10:27 -0400

    hello world

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,10 +4,7 @@ variable "region" {
   default     = "us-east-2"
 }
 
-variable "image" {
-  description = "Docker image to deploy (repository:tag)"
-  type        = string
-}
+
 
 variable "service_desired_count" {
   type    = number
