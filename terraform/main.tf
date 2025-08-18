ce1d7cf80dac0fc9cdb4167e68dbbaa0df2c1739
susabhavani987 <vijaychava101@gmail.com>
2025-08-18 10:46:41 -0400

    hello world

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,26 +21,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-# Security group allowing HTTP access
-resource "aws_security_group" "app_sg" {
-  name        = "app-sg"
-  description = "Allow HTTP access"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 # CloudWatch log group
 resource "aws_cloudwatch_log_group" "docker_logs" {
