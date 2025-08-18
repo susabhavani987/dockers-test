6451e4d8fa67166e0333ed273c757993c6774552
susabhavani987 <vijaychava101@gmail.com>
2025-08-18 09:19:54 -0400

    hello world

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
-provider "aws" {
-  region = "us-east-2"  # or use a variable
+pprovider "aws" {
+  region = "us-east-2"
 }
 
 variable "image" {
@@ -7,28 +7,64 @@ variable "image" {
   type        = string
 }
 
+# Create a security group allowing HTTP (80) access
+resource "aws_security_group" "app_sg" {
+  name        = "app-sg"
+  description = "Allow HTTP access"
+  vpc_id      = data.aws_vpc.default.id  # Default VPC
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Fetch default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+# Create CloudWatch Log Group for Docker logs
+resource "aws_cloudwatch_log_group" "docker_logs" {
+  name              = "my-docker-logs1"
+  retention_in_days = 7
+}
+
+# Launch EC2 instance
 resource "aws_instance" "app_server" {
-  ami           = "ami-01de4781572fa1285"  # Amazon Linux 2 AMI (update for your region)
+  ami           = "ami-01de4781572fa1285"  # Amazon Linux 2 AMI
   instance_type = "t2.micro"
+  subnet_id     = data.aws_subnet_ids.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
-              yum install -y python3 docker
+              amazon-linux-extras install docker -y
               service docker start
               usermod -a -G docker ec2-user
 
-              # Pull and run Docker container with the image passed
+              # Pull and run Docker container
               docker pull ${var.image}
-			  docker run -d \
-               --log-driver=awslogs \
-               --log-opt awslogs-region=us-east-2 \
-               --log-opt awslogs-group=my-docker-logs1 \
-               --log-opt awslogs-stream=${var.image} \
+              docker run -d -p 80:5000 \
+                --log-driver=awslogs \
+                --log-opt awslogs-region=us-east-2 \
+                --log-opt awslogs-group=my-docker-logs1 \
+                --log-opt awslogs-stream=${var.image} \
                 ${var.image}
-
-              # Alternatively, if you want to run app.py directly (if copied)
-              # python3 /path/to/app.py
               EOF
 
   tags = {
