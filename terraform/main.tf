a0a7fefa8be5367c3af9a337aaa508eba90f4d50
susabhavani987 <vijaychava101@gmail.com>
2025-08-18 10:44:42 -0400

    hello world

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,17 +42,54 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
-# CloudWatch log group for Docker logs
+# CloudWatch log group
 resource "aws_cloudwatch_log_group" "docker_logs" {
   name              = "my-docker-logs1"
   retention_in_days = 7
 }
 
+# IAM Role for EC2 (Session Manager + CloudWatch)
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach Session Manager policy
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Attach CloudWatch policy
+resource "aws_iam_role_policy_attachment" "cw_attach" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+# Instance profile
+resource "aws_iam_instance_profile" "ec2_ssm_profile" {
+  name = "ec2-ssm-profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
+
 # EC2 instance
 resource "aws_instance" "app_server" {
-  ami                    = "ami-01de4781572fa1285" # Amazon Linux 2 AMI
+  ami                    = "ami-01de4781572fa1285" # Amazon Linux 2
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.app_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_ssm_profile.name
 
   user_data = <<-EOF
               #!/bin/bash
@@ -61,7 +98,6 @@ resource "aws_instance" "app_server" {
               service docker start
               usermod -a -G docker ec2-user
 
-              # Pull and run Docker container
               docker pull ${var.image}
               docker run -d -p 80:5000 \
                 --log-driver=awslogs \
@@ -76,6 +112,11 @@ resource "aws_instance" "app_server" {
   }
 }
 
+# Outputs
 output "instance_public_ip" {
   value = aws_instance.app_server.public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.app_server.id
 }
