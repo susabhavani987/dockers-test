53d411817f55f7431c39b5d45ff5a4e6066e43e6
susabhavani987 <vijaychava101@gmail.com>
2025-08-19 12:45:10 -0400

    hello commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,7 +50,7 @@ resource "aws_cloudwatch_log_group" "docker_logs" {
 
 # IAM Role for EC2 (SSM + CloudWatch)
 resource "aws_iam_role" "ec2_ssm_role" {
-  name = "ec2-ssm-role-terraform-unique3"
+  name = "ec2-ssm-role-terraform-unique4"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -80,7 +80,7 @@ resource "aws_iam_role_policy_attachment" "cw_attach" {
 
 # IAM Instance Profile
 resource "aws_iam_instance_profile" "ec2_ssm_profile" {
-  name = "ec2-ssm-profile-terraform-unique3"
+  name = "ec2-ssm-profile-terraform-unique4"
   role = aws_iam_role.ec2_ssm_role.name
 }
 
